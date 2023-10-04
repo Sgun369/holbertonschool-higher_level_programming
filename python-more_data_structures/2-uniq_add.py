bc620f2f2830ba4a5686c5e8b62989e60126e9b8
Sgun369 <souhaiebjebali@gmail.com>
2023-10-04 13:17:53 +0100

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique = set()
+    unique = []
     sum = 0
 
     for num in my_list:
         if num not in unique:
-            unique.add(num)
+            unique.append(num)
             sum += num
     return sum
