9f5921be0bf46c6cc342f8c2ec909a7afa2548b4
Sgun369 <souhaiebjebali@gmail.com>
2023-10-05 14:21:14 +0100

    fix pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len_a < 2:
         for i in range(2 - len_a):
             tuple_a += (0, )
-    
+
     if len_b < 2:
         for i in range(2 - len_b):
             tuple_b += (0, )
@@ -14,4 +14,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = tuple_a[1] + tuple_b[1]
 
     return (a, b)
-
