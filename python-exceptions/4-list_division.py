4fa2bb54bcae3f80786a1194390de9e3c8d2d43e
Sgun369 <souhaiebjebali@gmail.com>
2023-10-06 18:53:05 +0100

    Hot fix

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,9 @@
 #!usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    result = 0
+   
     for i in range(list_length):
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
