ebce3d0d90f32b196f30b05da78a1a19052171cd
Sgun369 <souhaiebjebali@gmail.com>
2023-10-09 17:49:20 +0100

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""writing a class Square that defines a square by:
+private instance attribute size
+instantiation with optional size: def __init__(self, size=0)"""
+
+
+class Square:
+    """create a Square"""
+    def __init__(self, size=0):
+        """intitilazing the square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """property def size(self): to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public instance method:
+        def area(self):
+        that returns the current square area"""
+        return (self.__size ** 2)
