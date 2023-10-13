158675e7a0d4db62dec521b34fced5d449daaa15
Sgun369 <souhaiebjebali@gmail.com>
2023-10-13 19:38:23 +0100

    6-max_integer_test.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -16,17 +16,26 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_max_integer_empty_list(self):
-        """
-        Test max_integer with an empty list
-        """
-        self.assert_IsNone(max_integer([]))
+    def test_max_integer_negative_values(self):
+        """Test max_integer with a list of negative values."""
+        self.assertEqual(max_integer([-5, -2, -8, -1]), -1)
 
     def test_max_integer_single_element(self):
         """
         test max_integer with a list containing a sinngle element.
         """
         self.assertEqual(max_integer([42]), 42)
+
+    def test_max_integer_mixed_values(self):
+        """Test max_integer with a list of mixed
+        positive and negative values."""
+        self.assertEqual(max_integer([-5, 2, -8, 10]), 10)
+
+    def test_max_integer_strings(self):
+        """Test max_integer with a list of strings."""
+        self.assertEqual(
+            max_integer(["apple", "banana", "cherry", "date"]), "date"
+            )
 
 
 if __name__ == '__main__':
