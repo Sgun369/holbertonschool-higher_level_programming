a89560cbc5de3de2087258922b19094624f8a811
Sgun369 <souhaiebjebali@gmail.com>
2023-10-13 13:50:26 +0100

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,23 +3,24 @@
 
 """
 
+
 def text_indentation(text):
     """
-    print the text with two new lines after each of the 
+    print the text with two new lines after each of the
     characters '.', '?', and ':'.
 
     parameters:
         tect(str): The input text.
-    
+
     Raises:
         TypeError: If the text is not a string.
     """
-    
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     for char in ".?:":
         text = text.replace(char, char + "\n\n")
-    list_lines = [lines.strip(' ') for lines in text.split('\n')]
-    revised = "\n".join(list_lines)
-    print(revised, end="")
+    line_without_spaces = [lines.strip(' ') for lines in text.split('\n')]
+    formatted_text = "\n".join(line_without_spaces)
+    print(formatted_text, end="")
