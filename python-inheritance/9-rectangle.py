4ca1321d13315a532b2bb075510241052b938634
Sgun369 <souhaiebjebali@gmail.com>
2023-10-18 11:26:02 +0100

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -29,4 +29,4 @@ class Rectangle(BaseGeometry):
         """"
         return a descirption of the rectangle
         """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return f"[Rectangle] {self.__width}/{self.__height}"
