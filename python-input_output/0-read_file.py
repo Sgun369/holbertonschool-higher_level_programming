6d383a94b949806bfbe99550658dc37f894ccf69
Sgun369 <souhaiebjebali@gmail.com>
2023-10-18 11:40:23 +0100

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,6 @@ def read_file(filename=""):
     a function that reads a text file (UTF8) and
     prints to stdout
     """
-    with open ('my_file_0.txt', mode='r', encoding="utf-8", ) as f:
+    with open('my_file_0.txt', mode='r', encoding="utf-8", ) as f:
         for line in f:
             print(line, end='')
