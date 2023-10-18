3513d908d34e8c645d5ecaba3da98015827e8c4f
Sgun369 <souhaiebjebali@gmail.com>
2023-10-18 11:39:39 +0100

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""
+Read file module
+"""
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and
+    prints to stdout
+    """
+    with open ('my_file_0.txt', mode='r', encoding="utf-8", ) as f:
+        for line in f:
+            print(line, end='')
