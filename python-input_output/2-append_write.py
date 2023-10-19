fca89daf02436071f798588fad981484e4734bae
Sgun369 <souhaiebjebali@gmail.com>
2023-10-19 13:45:14 +0100

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -9,5 +9,5 @@ def append_write(filename="", text=""):
     a function that appends a string at the end of
     a text file and returns the number of charachters added
     """
-    with open('filename', mode='a', encoding='utf-8') as f:
+    with open(filename, mode='a', encoding='utf-8') as f:
         return (f.write(text))
