c2053817ccfe5ea90f876f233897a204d7807289
Sgun369 <souhaiebjebali@gmail.com>
2023-10-27 10:18:29 +0100

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,6 +15,10 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
     @property
     def width(self):
         """getter method for width"""
