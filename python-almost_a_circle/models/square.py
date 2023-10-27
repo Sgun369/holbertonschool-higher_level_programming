4d9e8edd708e7e7c05d87a3fcd0052123873f969
Sgun369 <souhaiebjebali@gmail.com>
2023-10-27 19:24:34 +0100

    Square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -16,3 +16,19 @@ class Square(Rectangle):
         """returns a string representation of the class"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
+
+    @property
+    def size(self):
+        """"zize getter"""
+        return self.width
+    
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
+    
