c999fd2187534919905191861fa3a39a912c896e
Sgun369 <souhaiebjebali@gmail.com>
2023-10-27 09:24:00 +0100

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python3
+#!/use/bin/python3
 """Base class module"""
 
 
 class Base:
-    """class Base"""
+    """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """class constructor"""
+        """initializes the base class"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects
+            self.id = Base.__nb_objects
