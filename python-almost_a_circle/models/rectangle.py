60ad9c8d1572959982eb1b4a9b9618f2ddadd61d
Sgun369 <souhaiebjebali@gmail.com>
2023-10-27 09:28:22 +0100

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,50 +15,42 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    @property
+    def width(self):
+        """getter method for width"""
+        return self.__width
 
-@property
-def width(self):
-    """getter method for width"""
-    return self.__width
+    @width.setter
+    def width(self, value):
+        """setter method for width"""
+        self.__width = value
 
+    @property
+    def height(self):
+        """gette method for height"""
+        return self.__height
 
-@width.setter
-def width(self, value):
-    """setter method for width"""
-    self.__width = value
+    @height.setter
+    def height(self, value):
+        """setter method for height"""
+        self.__height = value
 
+    @property
+    def x(self):
+        """getter method for x"""
+        return self.__x
 
-@property
-def height(self):
-    """gette method for height"""
-    return self.__height
+    @x.setter
+    def x(self, value):
+        """setter method for x"""
+        self.__x = value
 
+    @property
+    def y(self):
+        """getter method for y"""
+        return self.__y
 
-@height.setter
-def height(self, value):
-    """setter method for height"""
-    self.__height = value
-
-
-@property
-def x(self):
-    """getter method for x"""
-    return self.__x
-
-
-@x.setter
-def x(self, value):
-    """setter method for x"""
-    self.__x = value
-
-
-@property
-def y(self):
-    """getter method for y"""
-    return self.__y
-
-
-@y.setter
-def y(self, value):
-    """setter method for y"""
-    self.__y = value
+    @y.setter
+    def y(self, value):
+        """setter method for y"""
+        self.__y = value
