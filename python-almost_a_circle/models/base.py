5fed2606ea1c2f03bc6d83299b503fa0f4c6a52e
Sgun369 <souhaiebjebali@gmail.com>
2023-10-29 13:17:15 +0100

    indent

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
             return "[]"
-        return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries, indent=2)
 
     @classmethod
     def save_to_file(cls, list_objs):
