b772a06590c188a500252db36c966fff62be0555
Sgun369 <souhaiebjebali@gmail.com>
2023-11-23 11:33:19 +0100

    16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- a script that lists all records of the table second_table
 SELECT score, name
-FROM second_tableWHERE name IS NOT NULL
+FROM second_table WHERE name IS NOT NULL
 ORDER BY score DESC, name;
