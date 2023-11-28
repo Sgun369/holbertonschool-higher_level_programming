34a95ce54caaced264c2cb6cad4deaea573b122e
Sgun369 <souhaiebjebali@gmail.com>
2023-11-28 10:38:59 +0100

    docstrings

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # connect to the MySQL dtabae using provided credentials.
+    """connect to the MySQL dtabae using provided credentials."""
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
@@ -12,14 +12,14 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3]
     )
-    # Create a cursor object to interact with the database.
+    """Create a cursor object to interact with the database."""
     cur = db.cursor()
 
-    # Execute a SQL query to select all stated from the 'stats' table.
+    """Execute a SQL query to select all stated from the 'stats' table.""""
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows returned by the SQL query.
+    """Fetch all the rows returned by the SQL query"""
     rows = cur.fetchall()
-    # print each row representing a state in the database.
+    """print each row representing a state in the database.""""
 for row in rows:
     print(row)
