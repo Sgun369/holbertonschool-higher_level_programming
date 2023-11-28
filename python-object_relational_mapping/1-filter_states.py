a1ea1d037f8a31ce4f667b39e94b24f2b59d2235
Sgun369 <souhaiebjebali@gmail.com>
2023-11-28 10:46:53 +0100

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """connect to the MySQL dtabae using provided credentials."""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    """Create a cursor object to interact with the database."""
+    cursor = db.cursor()
+
+    """Execute a SQL query to select all stated from the 'stats' table."""
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+
+    """Fetch all the rows returned by the SQL query"""
+    rows = cursor.fetchall()
+
+    """Prints rows where the second column of the result starts with 'N'"""
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
+    db.close()
